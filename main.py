c490ae77adafc8f28255d5201b14ca08e64b4269
Mike <ms53150@gmail.com>
2022-04-08 23:28:36 -0400

    Add ball class for movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
 
 # Screen setup
 screen = Screen()
@@ -12,29 +13,22 @@ screen.tracer(0)
 # Paddles
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+# Ball
+ball = Ball()
 
 # keystrokes
 screen.listen()
-
 screen.onkeypress(r_paddle.move_up, "Up")
 screen.onkeypress(r_paddle.move_down, "Down")
 screen.onkeypress(l_paddle.move_up, "w")
 screen.onkeypress(l_paddle.move_down, "s")
-
-# Ball
-ball = Turtle()
-ball.shape("circle")
-ball.color("blue")
-ball.penup()
-
 
 game_is_on = True
 while game_is_on:
     screen.update()
     # control speed of ball
     time.sleep(0.1)
-    # sets angle of ball and moves foward
-    ball.setheading(20)
-    ball.forward(20)
+    # sets angle of ball and moves forward
+    ball.move()
 
 screen.exitonclick()
